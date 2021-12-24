a3fb58abfb30e149aa57793f1c9e2cf67a042000
mahesh <madu140@gmail.com>
2021-12-24 08:56:52 +0530

    Block class modified

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
 class Block
 {
 	private:
-		string hash;
-		string previous_hash;
+//		static long long index = 0;
+		string *hash;
+		string *previous_hash;
+		
+		// current date/time based on current system - should change
+		time_t now;
+		
 	public :
+		
+		// for implementation purposes
+		Block(){
+//			index++;
+			this->now = time(0);
+		}
+		
+		Block(string previous_hash)
+		{
+			// allcate mem dynamically to hash n previous_hash
+			hash = new string;
+			previous_hash = new string;
+//			index++;
+			this->now = time(0);
+			*previous_hash = previous_hash;
+			cout << "block " << index << " created" << endl;
+			// TODO : calculate hash of this block
+		}
+		
 		void displayMyHash()
 		{
-			cout << MyHash << endl;
+			cout << hash << endl;
 		}
 };
 
 class Transaction{
 	
 	private:
-		double amount = 0.0;
+		double amount;
 		string originator;
 		string borrower;
 		
@@ -28,6 +53,11 @@ class Transaction{
 			amount = a;
 			originator = o;
 			borrower = b;
+		}
+		
+		// destructor - calls when freeing the memory with delete
+		~Transaction(){
+			cout << "Transaction closed gracefully." << endl;
 		}
 		
 		double getTransactionAmount();
@@ -43,6 +73,7 @@ double Transaction :: getTransactionAmount()
 {
 	return Transaction :: amount;
 }
+
 int main()
 {
 	//	dynamic object creation in heap
@@ -50,8 +81,11 @@ int main()
 	
 	cout << PTrnsction->getTransactionAmount() << endl;
 	
+	
 	Block transactionBlock;
 	
 	transactionBlock.displayMyHash();
+	
+	delete PTrnsction;
 	return 0;
 }
