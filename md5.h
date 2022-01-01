5bdf6776a7823d4d69a0872a964eb7ee09358cb8
mahesh <madu140@gmail.com>
2022-01-01 09:06:41 +0530

    md5 hash algorithm class added

---
+++ md5.h
@@ -0,0 +1,15 @@
+#ifndef MD5_H
+#define MD5_H
+
+#include "clib/cryptlib.h"
+#include "clib/md5.h" // from https://cryptopp.com/
+#include <iostream>
+#include <string>
+#include "Hash.h"
+
+class MD5 : public Hash
+{
+	
+}; 
+
+#endif // MD5_H
