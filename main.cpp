066fea8b948639c6ccb0850e4984d6e2468ef228
newark <madu140@gmail.com>
2022-01-30 08:24:22 +0530

    refactored

--- main.cpp
+++ main.cpp
@@ -1,54 +1,26 @@
 #include <iostream>
-#include <string>
 #include <ctime>
+#include <Transaction.h>
 
 using namespace std;
 
 #include "Block.h"
 
-class Transaction{
-	
-	private:
-		double amount;
-		string originator;
-		string borrower;
-		
-	public:
-		
-		Transaction(double a, string o, string b){
-			amount = a;
-			originator = o;
-			borrower = b;
-		}
-		
-		// destructor - calls when freeing the memory with delete
-		~Transaction(){
-			cout << "Transaction closed gracefully." << endl;
-		}
-		
-		double getTransactionAmount();
-		
-		void setAmount(double amount)
-		{
-			this->amount = amount;
-		}
-		
-};
-
-double Transaction :: getTransactionAmount()
-{
-	return Transaction :: amount;
-}
 
 int main()
 {
 	//	dynamic object creation in heap
-	Transaction *PTrnsction = new Transaction(100.00, "sol", "eth");
-	
-	cout << PTrnsction->getTransactionAmount() << endl;
-	Block transactionBlock;
-	transactionBlock.displayMyHash();
-	
-	delete PTrnsction;
-	return 0;
+	try
+	{
+		Transaction *PTrnsction = new Transaction(100.00, "sol", "eth");
+		cout << PTrnsction->getTransactionAmount() << endl;
+		Block transactionBlock;
+		transactionBlock.displayMyHash();
+		delete PTrnsction;
+		return 0;
+	}
+	catch (const std::bad_alloc &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
