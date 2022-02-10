20510c3672a76a095bf0fdc87070a5fef9486e71
newark <madu140@gmail.com>
2022-02-10 07:40:59 +0530

    md5 hash class fix

--- md5.cpp
+++ md5.cpp
@@ -1,23 +1,15 @@
 #include "md5.h"
 #include <string>
 #include <iostream>
-
 #include "clib/cryptlib.h"
 #include "clib/md5.h"
 
-// using namespace std;
+using namespace std;
 
-MD5::addData(std::string* data){
+void MD5::addData(string *data){
 	this->data = data;
 };
 
-MD5::Hash(std::string* data){
-	this->data = data;
-};
-
-MD5::Hash(){
-};
-
-MD5::setData(){
+void MD5::setData(string *data){
 	
 }
