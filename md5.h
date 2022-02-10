ad23a4b3f86b42226d79dc9781db68cde06433fb
newark <madu140@gmail.com>
2022-02-10 07:41:18 +0530

    md5 hash class fix

--- md5.h
+++ md5.h
@@ -5,10 +5,15 @@
 #include <string>
 #include "Hash.h"
 
+
+using namespace std;
+
 class MD5 : public Hash
 {
 	MD5() : Hash(){};
-	MD5(std::string *data) : Hash(data){};
+	MD5(string *data) : Hash(data){};
+	void addData(string* data);
+	void MD5::setData(string* data);
 }; 
 
 #endif // MD5_H
