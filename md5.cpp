4f5729d656a545d1689bef133ada228d458f96ed
newark <madu140@gmail.com>
2022-02-14 07:26:38 +0530

    Hash fixed

--- md5.cpp
+++ md5.cpp
@@ -1,8 +1,22 @@
 #include "md5.h"
 #include <string>
 #include <iostream>
+#include "clib/filters.h"
 
 using namespace std;
+
+
+#include "clib/hex.h"
+#include "clib/files.h"
+
+// The HexEncoder encodes bytes into base 16 encoded data. The partner decoder is a HexDecoder.
+// A FileSink allows you to write data to a file using a BufferedTransformation
+// BufferedTransformation is the basic unit of data flow in Crypto++. 
+// An object which derives from BufferedTransformation can participate in Pipelining. 
+// Pipelining is a paradigm in Crypto++ which allows data to flow from a Source to a Sink. 
+// Each filter in the pipeline transforms the data in some way in the in route to the Sink, 
+// and BufferedTransformation provides the interface across objects.
+HexEncoder encoder(new FileSink(std::cout));
 
 void MD5::addData(string *data){
 	this->data = data;
@@ -23,6 +37,7 @@ void MD5::getHash(){
 	hash.Final((CryptoPP::byte*)&data_digest[0]);
 
 	std::cout << "Message: " << *data << std::endl;
-	std::cout << "Digest: " << data_digest << std::endl;
+	std::cout << "Digest: " ;
+	StringSource(data_digest, true, new Redirector(encoder));
 	std::cout << std::endl;
 }
