a41c713678a721e532c6a376804a5831f29b6c6b
newark <madu140@gmail.com>
2022-02-17 07:33:00 +0530

    update

--- main.cpp
+++ main.cpp
@@ -10,24 +10,33 @@ using namespace std;
 
 // for debugging of new (this is overloaded new) (logger for new)
 // overloaded new and delete for logging
-#ifdef _DEBUG /* only build with debug */
+
+#ifdef __DEBUG__ /* only build with debug */
+
 void* operator new(size_t size, const char* file, int line){
 	std::string fullpath = file;
-	std::string filename = fullpath.substr(fullpath.find_last_of("\//"));
-	printf("new F:%s L:%d \n",filename.c_str(), line);
+	// std::string filename = fullpath.substr(fullpath.find_last_of("\\//"));
+	printf("new F:%s L:%d \n",fullpath.c_str(), line);
 	return malloc(size);
 }
+
 void log_delete(const char* file, int line){
 	std::string fullpath = file;
-	std::string filename = fullpath.substr(fullpath.find_last_of("\//"));
-	printf("new F:%s L:%d \n",filename.c_str(), line);
+	// std::string filename = fullpath.substr(fullpath.find_last_of("\\//"));
+	printf("delete F:%s L:%d \n",fullpath.c_str(), line);
 }
+
 void operator delete(void* p, const char* file, int line){
 	free(p);
 }
-/* __FILE__ is a preprocessor macro that expands to full path to the current file. __FILE__ is useful when generating log statements, error messages intended for programmers, when throwing exceptions, or when writing debugging code. */
+
+/* __FILE__ is a preprocessor macro that expands to full path to the current file. 
+__FILE__ is useful when generating log statements, error messages intended for programmers, when throwing exceptions,
+ or when writing debugging code. */
+
 #define new new(__FILE__,__LINE__)
 #define delete log_delete(__FILE__,__LINE__); delete
+
 #endif
 
 int main()
@@ -35,21 +44,30 @@ int main()
 	//	Bad allocation problem resolved
 	try
 	{
-		// use shared_ptr instead of new (it deletes when ptr no longer in use)
+
 		Transaction *PTrnsction = new Transaction(100.00, "sol", "eth");
-		shared_ptr<Transaction> spTransaction = make_shared<Transaction>(100.00, "sol", "eth");
+
 		cout << PTrnsction->getTransactionAmount() << endl;
-		Block transactionBlock;
-		transactionBlock.displayMyHash();
+
+		delete PTrnsction;
+
+		// use shared_ptr instead of new (it deletes when ptr no longer in use)
+		shared_ptr<Transaction> spTransaction = make_shared<Transaction>(100.00, "sol", "eth");
+
+		// Block transactionBlock;
+
+		// transactionBlock.displayMyHash();
+
 		string string_data = "jibberish";
 		Hash *md5;
 		MD5 s(&string_data);
 		md5 = &s;
 		md5->getHash();
-		delete PTrnsction;
+
 
 		return 0;
 	}
+
 	catch (const bad_alloc &e)
 	{
 		cerr << e.what() << '\n';
