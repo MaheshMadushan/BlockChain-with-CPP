f3d6ac775f909a562cb5d64cc6b98d4342463eea
newark <madu140@gmail.com>
2022-02-19 08:29:25 +0530

    os configs added

---
+++ os_conf.h
@@ -0,0 +1,15 @@
+#if (defined(DEBUG) || defined(__DEBUG__)) || (defined(NDK_DEBUG) && (NDK_DEBUG > 0))
+# undef BLOCKCHAIN_DEBUG
+# define BLOCKCHAIN_DEBUG 1
+#endif
+
+// Unix and Linux platforms
+#if defined(__unix__) || defined(__MACH__) || defined(__NetBSD__) || defined(__sun)
+#define BLOCKCHAIN_UNIX_AVAILABLE
+#endif
+
+
+// Cygwin/Newlib requires _XOPEN_SOURCE=600
+#if defined(BLOCKCHAIN_UNIX_AVAILABLE)
+# define UNIX_SIGNALS_AVAILABLE 1
+#endif
