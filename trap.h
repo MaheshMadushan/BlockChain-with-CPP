064020a266a765e161b26763ef927ef46f6ffa76
newark <madu140@gmail.com>
2022-02-20 08:23:14 +0530

    debugging functions

---
+++ trap.h
@@ -0,0 +1,20 @@
+#include <sstream>
+#include "os_conf.h"
+#define    SIGTRAP        5    /* Trace/breakpoint trap.  */
+#define    SIGKILL        9    /* Killed.  */
+#if defined(BLOCKCHAIN_DEBUG)
+# 	if defined(UNIX_SIGNALS_AVAILABLE) || defined(__CYGWIN__)
+	#	define BLOCKCHAIN_ASSERT(exp) { 												\
+		if (!(exp)) 																	\
+		{ 																				\
+			std::ostringstream oss; 													\
+			oss << "Assertion failed: " << __FILE__ << "(" 								\
+				<< __LINE__ << "): " 													\
+				<< __func__ << std::endl; 												\
+			std::cout << std::flush; 													\
+			std::cerr << oss.str(); 													\
+			raise(SIGTRAP); 															\
+		} 																				\
+	}																					
+#endif // Unix
+#endif // Debug Enabled
