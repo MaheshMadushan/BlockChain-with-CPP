44d11346be1d5b0f9b09fe50ad448c847dcfd8f1
newark <madu140@gmail.com>
2022-02-20 08:12:26 +0530

    Os configs

--- os_conf.h
+++ os_conf.h
@@ -3,12 +3,21 @@
 # define BLOCKCHAIN_DEBUG 1
 #endif
 
+
+
+
+
 // Unix and Linux platforms
 #if defined(__unix__) || defined(__MACH__) || defined(__NetBSD__) || defined(__sun)
 #define BLOCKCHAIN_UNIX_AVAILABLE
 #endif
 
 
+
+
+
+// Handle processes using signals
+// eg : pause a process
 // Cygwin/Newlib requires _XOPEN_SOURCE=600
 #if defined(BLOCKCHAIN_UNIX_AVAILABLE)
 # define UNIX_SIGNALS_AVAILABLE 1
