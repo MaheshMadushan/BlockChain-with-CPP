81b2ec947f91d97ae4100829639f3943f4d0c2b5
newark <madu140@gmail.com>
2022-02-23 09:27:35 +0530

    added blockchain classes and tofdos

---
+++ blockchain.h
@@ -0,0 +1,19 @@
+#ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
+#include "Block.h"
+#include "Hash.h"
+#include "md5.h"
+
+
+
+
+// TODO : create genesis block
+// TODO : hash a block
+// TODO : create genesis block
+// TODO : data structure to keep blocks (prefer hash table for now)
+// TODO : create block
+
+
+
+
+#endif // BLOCKCHAIN_H
